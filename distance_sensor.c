6c4a2246cbe1c11cd88215b5619410dc7ba40986
kurtisman <mank@oregonstate.edu>
2026-03-02 13:32:22 -0800

    Update distance_sensor.c

--- distance_sensor.c
+++ distance_sensor.c
@@ -1,5 +1,5 @@
 //Microcontroller code below this line
 
-//Distance Sensor code below this line
+//Distance Sensor code below this line. Distance value will be stored as variable __ with type __
 
-//UI code below this line
+//UI code below this line. This block will accept variable __ with type __
