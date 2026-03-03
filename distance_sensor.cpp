c7d2ed206008d3ab3ea996303959c79d659e715f
Gregory Samarxhi <samarxhg@oregonstate.edu>
2026-03-03 11:24:35 -0800

    Converted to cpp file instead of c file

---
+++ distance_sensor.cpp
@@ -0,0 +1,5 @@
+//Microcontroller code below this line
+
+//Distance Sensor code below this line. Distance value will be stored as variable distance_sensed with type double
+
+//UI code below this line. This block will accept variable distance_sensed with type double
